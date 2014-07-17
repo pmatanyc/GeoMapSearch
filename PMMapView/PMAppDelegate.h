c2c864cc0ce9c222394496c2b3808bfbd441268a
Paola Mata Maldonado <pmatanyc@gmail.com>
2014-07-17 14:20:43 -0400

    Initial Commit

---
+++ PMMapView/PMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PMAppDelegate.h
+//  PMMapView
+//
+//  Created by Paola Mata Maldonado on 7/17/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PMMapView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PMMapView
+//
+//  Created by Paola Mata Maldonado on 7/17/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PMAppDelegate class]));
+    }
+}
